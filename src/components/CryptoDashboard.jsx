1471fa571f79a86fb71ba526476855c448098526
Austen Sorochak <austensorochak@gmail.com>
2024-10-02 16:42:40 -0700

    implement lazy loading for CryptoChart and Legend components

--- src/components/CryptoDashboard.jsx
+++ src/components/CryptoDashboard.jsx
@@ -1,8 +1,11 @@
-import CryptoChart from "./CryptoChart";
+import React, { Suspense, lazy } from "react";
 import { Box, Typography, Container } from "@mui/material";
 import { calculateAvgVolume, calculateQoQChange } from "../utils/calculations";
-import Legend from "./Legend";
 import data from "../data/data.json";
+
+// Lazy load the CryptoChart and Legend components
+const CryptoChart = lazy(() => import("./CryptoChart"));
+const Legend = lazy(() => import("./Legend"));
 
 const CryptoDashboard = () => {
   // Define the date range for Q2 average volume calculation
@@ -78,11 +81,14 @@ const CryptoDashboard = () => {
             marginTop: 4,
           }}
         >
-          {/* Area Chart */}
-          <CryptoChart data={data} />
+          {/* Suspense ensures that the components are displayed only once they are loaded */}
+          <Suspense fallback={<div>Loading chart...</div>}>
+            {/* Area Chart */}
+            <CryptoChart data={data} />
 
-          {/* Legend */}
-          <Legend avgVolumes={avgQ2Data} />
+            {/* Legend */}
+            <Legend avgVolumes={avgQ2Data} />
+          </Suspense>
         </Box>
       </Box>
     </Container>
